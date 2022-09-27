3d7154578cd7bcd3b3151bc0d9dde439d7406715
Serhii Kaushnian <skaushnjan@gmail.com>
2022-09-27 10:44:05 +0200

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,15 +5,29 @@ import Filter from '../Filter/Filter';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import { AppWrapper, Wrapper } from './App.styled';
 export default class App extends Component {
+  LOCAL_STORAGE_KEY = 'contactList';
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem(
+        this.LOCAL_STORAGE_KEY,
+        JSON.stringify(this.state.contacts)
+      );
+    }
+  }
+  componentDidMount() {
+    const localContacnts = localStorage.getItem(this.LOCAL_STORAGE_KEY);
+    const parcedLocalContacts = JSON.parse(localContacnts);
+    if (parcedLocalContacts) {
+      this.setState({
+        contacts: parcedLocalContacts,
+      });
+    }
+  }
 
   isContactlreadyExist = ({ name }) => {
     const { contacts } = this.state;
